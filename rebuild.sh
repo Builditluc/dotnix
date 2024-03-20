5a7780f05677b04bb1ff64843f87955ab395c013
Builditluc <37375448+Builditluc@users.noreply.github.com>
2024-03-20 17:39:24 +0100

    260 current  2024-03-20 17:39:21  24.05pre-git            6.6.21                                            *

--- rebuild.sh
+++ rebuild.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+if git diff --quiet; then
+  echo "No changes detected, exiting."
+  exit 0
+fi
+
 git add .
 git diff --staged
 
 echo "building NixOS Configuration"
 sudo nixos-rebuild switch --flake .# &> nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+rm nixos-switch.log
 
 current=$(nixos-rebuild list-generations | grep current)
 current_num=$(echo $current | awk '{print $1}')
 
-rm nixos-switch.log
 
 git commit -am "$current"
 notify-send "NixOS build of Generation $current_num was successful!"
