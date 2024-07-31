ba83e261c50e01877f46db59b04550c9c73fd1b7
Builditluc <git@builditluc.eu>
2024-07-31 19:51:13 +0200

    319 current  2024-07-31 19:50:39  24.05pre-git   6.9.6                                             *

--- hm/git/default.nix
+++ hm/git/default.nix
@@ -9,7 +9,7 @@
     enable = true;
     
     userName = "Builditluc";
-    userEmail = "37375448+Builditluc@users.noreply.github.com";
+    userEmail = "git@builditluc.eu";
 
     signing.key = "62F515FE2B41AE9ED18792A86139D472C7AE6CB0";
     signing.signByDefault = true;
