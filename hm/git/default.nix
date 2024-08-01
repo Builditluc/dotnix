771d37e39ea0c45077ee2bacef48bb1ed7eb8688
Builditluc <git@builditluc.eu>
2024-08-01 15:52:07 +0200

    321 current  2024-08-01 15:50:24  24.05pre-git   6.9.6                                             *

--- hm/git/default.nix
+++ hm/git/default.nix
@@ -14,6 +14,22 @@
     signing.key = "62F515FE2B41AE9ED18792A86139D472C7AE6CB0";
     signing.signByDefault = true;
 
+    aliases.lg = "lg1";
+    aliases.lg1 = "lg1-specific --all";
+    aliases.lg2 = "lg2-specific --all";
+    aliases.lg3 = "lg3-specific --all";
+
+    aliases.lg1-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold
+    blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)-
+    %an%C(reset)%C(auto)%d%C(reset)'";
+    aliases.lg2-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold
+    blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''
+    %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+    aliases.lg3-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold
+    blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold
+    cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''
+    %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'";
+
     extraConfig = {
       core.editor = "nvim";
     };
